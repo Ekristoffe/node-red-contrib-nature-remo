e374c6333bffa36359b98793a7b5744e2d606ce4
masayoshikuroda <33488154+masayoshikuroda@users.noreply.github.com>
2018-01-27 17:00:38 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+var Fs = require('fs');
+var Request = require('request');
+
+function post_signal(signal_id, token) {
+  var options = {
+	  url: "https://api.nature.global/1/signals/" + signal_id + "/send",
+    headers: {
+      "Authorization": "Bearer " + token
+    }
+  }
+  Request.post(options, function(error, response, body) {});
+}
+
+function get_signal_id(appliances, nickname, name) {
+  var signal_id = ''
+  var file = Fs.readFileSync(appliances, 'utf8')
+  var json = JSON.parse(file)
+  for (var i=0; i<json.length; i++) {
+    if (json[i]['nickname'] != nickname) {
+      continue
+    }
+    for(var j=0; j<json[i]['signals'].length; j++) {
+      if (json[i]['signals'][j]['name'] == name) {
+        signal_id = json[i]['signals'][j]['id']
+      }
+    }
+  }
+  return signal_id
+}
+
+module.exports = function(RED) {
+    function NatureRemoNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on('input', function(msg) {
+	    var nickname = msg.payload.nickname
+	    var name = msg.payload.name
+	    var signal_id = get_signal_id(config.appliances, nickname, name)
+	    post_signal(signal_id, config.token)
+        });
+    }
+    RED.nodes.registerType("nature-remo", NatureRemoNode);
+}
