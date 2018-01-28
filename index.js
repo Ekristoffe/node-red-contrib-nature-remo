1db770a3e19fe88dcc756d5ffe57a545fca45397
Masayoshi Kuroda <kuroda.masayoshi@gmail.com>
2018-01-28 12:35:23 +0900

    credentialサポート

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var Request = require('request');
 
 function post_signal(signal_id, token) {
   var options = {
-	  url: "https://api.nature.global/1/signals/" + signal_id + "/send",
-    headers: {
-      "Authorization": "Bearer " + token
-    }
+    url: "https://api.nature.global/1/signals/" + signal_id + "/send",
+    headers: { "Authorization": "Bearer " + token }
   }
-  Request.post(options, function(error, response, body) {});
+  Request.post(options, function(error, response, body) {
+    if (error) {
+      console.error('POST failed:', error);
+    }
+  });
 }
 
 function get_signal_id(appliances, nickname, name) {
@@ -29,6 +31,12 @@ function get_signal_id(appliances, nickname, name) {
 }
 
 module.exports = function(RED) {
+    function NatureTokenNode(config) {
+	RED.nodes.createNode(this, config);
+    }
+	
+    RED.nodes.registerType('nature-token', NatureTokenNode, { credentials: { token: { type: "text" }}});
+
     function NatureRemoNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
@@ -36,8 +44,10 @@ module.exports = function(RED) {
 	    var nickname = msg.payload.nickname
 	    var name = msg.payload.name
 	    var signal_id = get_signal_id(config.appliances, nickname, name)
-	    post_signal(signal_id, config.token)
+            var token = RED.nodes.getNode(config.token).credentials.token
+	    post_signal(signal_id, token)
         });
     }
+
     RED.nodes.registerType("nature-remo", NatureRemoNode);
 }
