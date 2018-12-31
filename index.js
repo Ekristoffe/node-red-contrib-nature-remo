74b4b4b84bd3de8a13b1e83ffe914d5f74a8f607
Masayoshi Kuroda <kuroda.masayoshi@gmail.com>
2018-12-31 10:23:15 +0900

    modified:   index.html 	modified:   index.js 	modified:   package.json 	modified:   test.js 	.gitignore

--- index.js
+++ index.js
@@ -1,67 +1,10 @@
 var Fs = require('fs');
 var Request = require('request');
-
-function setTimeoutPromise(delay) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            resolve();
-        }, delay);
-    });
-}
-
-function post_signal_promise(token, signal_id) {
-    var options = {
-        url: "https://api.nature.global/1/signals/" + signal_id + "/send",
-        headers: { "Authorization": "Bearer " + token }
-    };
-    return new Promise(function(resolve, reject) {
-        Request.post(options, function(error, response, body) {
-            if (error) {
-                console.error('POST failed:', error);
-	        reject(error);
-            } else {
-                resolve();
-            }
-        });
-    });
-}
-
-function post_signals_promise(token, signal_ids, delay) {
-    return new Promise(function(resolve, reject) {
-        var p = post_signal_promise(token, signal_ids[0]);
-	signal_ids.slice(1).forEach(function(signal_id) {
-	    p = p.then(() => setTimeoutPromise(delay));
-	    p = p.then(() => post_signal_promise(token, signal_id));
-        });
-        p.then(() => resolve());
-    });
-}
-
-function get_signal_id(json, nickname, name) {
-    var signal_id = '';
-    json.filter(function(appliance) {
-        return appliance['nickname'] == nickname;
-    }).forEach(function(appliance) {
-        appliance['signals'].filter(function(signal) {
-	    return signal['name'] == name;
-	}).forEach(function(signal) {
-	    signal_id = signal['id'];
-	});
-    })
-    return signal_id
-}
-
-function get_signal_ids(appliances, commands) {
-    var file = Fs.readFileSync(appliances, 'utf8');
-    var json = JSON.parse(file);
-    return  commands.map(function(command) {
-        return get_signal_id(json, command.nickname, command.name);
-    });
-}
+var Remo = require('./remo.js');
 
 module.exports = function(RED) {
     function NatureTokenNode(config) {
-	RED.nodes.createNode(this, config);
+	    RED.nodes.createNode(this, config);
     }
 	
     RED.nodes.registerType('nature-token', NatureTokenNode, { credentials: { token: { type: "text" }}});
@@ -69,16 +12,16 @@ module.exports = function(RED) {
     function NatureRemoNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        node.on('input', function(msg) {
-	    var signal_ids = get_signal_ids(config.appliances, msg.payload.commands);
+        node.on('input', async function(msg) {
+	        var commands = Remo.append_signal_ids(config.appliances, msg.payload.commands);
             var token = RED.nodes.getNode(config.token).credentials.token;
-	    post_signals_promise(token, signal_ids, config.delay)
-	        .then(function() {
-		    msg.payload = "done";
-		    node.send(msg);
-                }).catch(function(error) {
-	            node.error("HTTP error" + error);
-	        });
+            try {
+                await Remo.post_commands_promise(token, commands);
+                msg.payload = "done";
+                node.send(msg);
+            } catch(error) {
+                node.error("POST command error" + error);
+            }
         });
     }
 
--- test.js
+++ test.js
@@ -1,1 +1,44 @@
+var remo = require('./remo.js');
+var Fs = require('fs');
 
+var tokenPath = "./token.txt";
+var token = Fs.readFileSync(tokenPath, 'utf8').trim();
+console.log(token);
+
+var appliancesPath = "./appliances.json";
+var appliances = Fs.readFileSync(appliancesPath, 'utf8');
+var json = JSON.parse(appliances);
+
+async function testAll() {
+    var nickname = "テレビ";
+    var name = "情報";
+    var signal_id = remo.get_signal_id(json, nickname, name);
+    console.log('test post_commands_promise done.');
+    console.log(signal_id);
+
+    var commands = [
+        { nickname: nickname, name: name, before_wait: 1000, after_wait: 3000 }, 
+        { nickname: nickname, name: name, before_wait: 1000, after_wait: 3000 },       
+    ];
+    commands = remo.append_signal_ids(appliancesPath, commands);
+    console.log('test append_signal_ids done.');
+    console.log(commands[0].signal_id);
+
+    await remo.set_timeout_promise(1000)
+    console.log('test set_timeout_promise done.');
+
+    var options = {url: 'http://www.google.com', method: 'GET'}
+    var body = await remo.post_promise(options);
+    console.log('test post_promise done.');
+
+    await remo.post_signal_promise(token, signal_id);
+    console.log('test post_signal_promise done.');
+
+    await remo.post_command_promise(token, commands[0]);
+    console.log('test post_command_promise done.');
+
+    await remo.post_commands_promise(token, commands);
+    console.log('test post_commands_promise done.');
+}
+
+testAll();
